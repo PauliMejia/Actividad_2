6b12510819be893c01f32023067bef671a24da55
PauliMejia <paulimata.1128@gmail.com>
2024-07-01 19:48:49 -0600

    Add files via upload

---
+++ ESTRUCTURA-CONTROL/Practica1.js
@@ -0,0 +1,12 @@
+//Realizar un programa que indique si el numero ingresado es positivo o negativo
+
+
+ let numero = 5; // Asigna un valor a la variable numero
+
+if (numero > 0) {
+    console.log("el numero es positivo");
+} else if (numero < 0) {
+    console.log("el numero es negativo");
+} else {
+    console.log("el numero es cero");
+}
---
+++ ESTRUCTURA-CONTROL/switch.js
@@ -0,0 +1,27 @@
+//Switch
+
+let q1 = 10;
+
+switch (q1){
+    case 10:
+    console.log("Buen trabajo");
+    Break;
+    case 9:
+    console.log("Casi perdecto");
+    Break;
+    case 8:
+    console.log("Bien supongo");
+    Break;
+    case 7:
+    console.log("Estudia mas");
+    Break;
+    case 6:
+    console.log("esfuerzate");
+    Break;
+    case 5:
+    console.log("reprobado");
+    Break;
+    default:
+        console.log("sytax error");
+        break;
+}
